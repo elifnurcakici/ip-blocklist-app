f1b0c3ea36fb5af6efa5d430429d5e581b0218ec
Elif Nur Çakıcı <elifncakici@gmail.com>
2026-04-02 15:50:44 +0300

    Refactor localStorage handling for IPs

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import {useEffect, useState} from "react"
 function App(){
   const[ip, setIp] = useState<string>("")  
   const[ips,setIps] = useState<string[]>(() => {
-    const storedIps = localStorage.getItem("ips") // localStorage içindeki "ips" verisini alır
-
-    return storedIps ? JSON.parse(storedIps) : []// veri varsa parse edip array olarak döndür yoksa boş array döndür
+    const storedIps = localStorage.getItem("ips") 
+    return storedIps ? JSON.parse(storedIps) : []
   })
 
   const [error, setError] = useState<string>("") 
@@ -13,16 +12,16 @@ function App(){
   const ipRegex = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/
 
   useEffect(() => {
-    const storedIps = localStorage.getItem("ips") // localStorage içinden "ips" anahtarına ait veriyi alır.
+    const storedIps = localStorage.getItem("ips")
 
-    if(storedIps){ // Eğer storeIps null değilse
-      setIps(JSON.parse(storedIps)) // localStorage'dan alınan veri string formatında olduğu için JSON.parse ile tekrar array'e çeviriyoruz.
+    if(storedIps){
+      setIps(JSON.parse(storedIps)) 
     }
-  }, []) // Bu effect component sadece component ilk yüklendiğinde 1 kez çalışır
+  }, [])
 
   useEffect(() => {
-    localStorage.setItem("ips", JSON.stringify(ips)) // array string'e çevrilir ve ips statei her değiştiğinde localStorage'a kaydedilir
-  }, [ips]) // ips değişirse bu effect tekrar çalışır.
+    localStorage.setItem("ips", JSON.stringify(ips))
+  }, [ips]) 
   function  handleAddIP(){
     setError("") 
     if(ip.trim() ==="") return 
@@ -48,28 +47,28 @@ function App(){
   }
 
   return(
-    <div className="min-h-screen bg-slate-100 flex items-start justify-center pt-20"> {/*min-h-screen : ekranın tamamını kapla, bg-slate-100 : arka plan rengi, flex:flexbox aktif, items-start:üstte hizala, justify-center: yatay ortala, pt-20: üstten padding*/}
-      <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-md space-y-4">{/*Kart görünümü- bg-white: beyaz arka plan, p-6: padding, rounded-xl:köşeleri yuvarla, shadow-md:hafif gölge, w-full: genişliği full, max-w-md: maksimum genişlik*/}
+    <div className="min-h-screen bg-slate-100 flex items-start justify-center pt-20"> 
+      <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-md space-y-4">
         <h1 className="text-2xl font-bold text-center">IP Blocklist</h1>
         <input
         type="text"
         placeholder="Enter IP Address"
         value={ip}
-        onChange={(e) => setIp(e.target.value)} // kullanıcı yazdıkça state güncellenir.
-        className="border p-2 rounded w-full" // border:kenarlık, p-2:padding, rounded:köşeleri yuvarlama, w-full:genişliği full
+        onChange={(e) => setIp(e.target.value)} 
+        className="border p-2 rounded w-full" 
         ></input>
         {error && (
           <p className="text-red-500 text-sm">
             {error}
           </p>
         )}
-        <button onClick={handleAddIP} // tıklanınca fonksiyon çalışır
+        <button onClick={handleAddIP}
           className="bg-blue-500 text-white py-2 rounded w-full"
         >Add IP</button>
         <ul className="space-y-2">
           {ips.map((item,index) => ( 
-            <li key={index} className="bg-gray-100 p-2 rounded flex justify-between items-center"> {/* React için uniq key, index kullanılarak benzersiz bir key oluşturulur.*/}
-            <span>{item}</span> {/*ips listesindeki her bir ip adresi için bir liste oluşturulur. map fonksiyonu ile her bir ip adresi item olarak alınır ve index de benzersiz bir key olarak kullanılır*/}
+            <li key={index} className="bg-gray-100 p-2 rounded flex justify-between items-center"> 
+            <span>{item}</span> 
           <button
             onClick={() => handleDeleteIP(item)}
             className="bg-red-500 text-white px-3 py-1 rounded"
